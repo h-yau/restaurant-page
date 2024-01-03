1460682a499e5e1522e8f794c857878f9833e585
nzxzn <99358826+nzxzn@users.noreply.github.com>
2024-01-03 14:22:48 -0800

    Add page sections

--- src/index.js
+++ src/index.js
@@ -1,61 +1,4 @@
-import './styles.css';
-
-function addNavContainer() {
-    const navContainer = document.createElement('header');
-    navContainer.classList.add('nav-container');
-    
-    const sideNav = document.createElement('div');
-    sideNav.classList.add('side-nav');
-
-    navContainer.appendChild(sideNav);
-
-    const logo = document.createElement('a');
-    logo.href = "/";
-    logo.classList.add('logo');
-    
-    sideNav.appendChild(logo);
-
-    const logoText = document.createElement('p');
-    logoText.textContent = 'Ramen House';
-    logoText.classList.add('logo-text');
-
-    logo.appendChild(logoText);
-
-    const siteLocation = document.createElement('div');
-    siteLocation.classList.add('site-location');
-
-    sideNav.appendChild(siteLocation);
-
-    const googleMapLink = document.createElement('a');
-    googleMapLink.href = "https://maps.app.goo.gl/GD9BNZ7fBhJxeE9u6";
-
-    siteLocation.appendChild(googleMapLink);
-
-    const address = ['111 Fake St,', 'San Francisco, CA 12345'];
-
-    for (let i = 0; i < address.length; i++) {
-        const p = document.createElement('p');
-        p.textContent = address[i];
-        googleMapLink.appendChild(p);
-    }
-    
-    const subSideNav = document.createElement('div');
-    subSideNav.classList.add('sub-side-nav');
-    
-    sideNav.appendChild(subSideNav);
-
-    const listItems = ['RESERVE', 'MENU', 'OUR STORY', 'HOURS & LOCATION', 'JOIN OUR TEAM'];
-
-    for (let i = 0; i < listItems.length; i++) {
-        const button = document.createElement('button');
-        button.classList.add(listItems[i].replaceAll(' ', '-').toLowerCase());
-        button.textContent = listItems[i];
-
-        subSideNav.appendChild(button);
-    }
-
-    return navContainer;
-}
+import preparePg from './nav.js';
 
 function addMain() {
 
@@ -88,6 +31,5 @@ function addMain() {
 
 
 
-document.getElementById('content').appendChild(addNavContainer());
+preparePg();
 document.getElementById('content').appendChild(addMain());
-document.getElementById('content').classList.add('window');
---
+++ src/nav.js
@@ -0,0 +1,63 @@
+import './styles.css';
+
+function addNavContainer() {
+    const navContainer = document.createElement('header');
+    navContainer.classList.add('nav-container');
+    
+    const sideNav = document.createElement('div');
+    sideNav.classList.add('side-nav');
+
+    navContainer.appendChild(sideNav);
+
+    const logo = document.createElement('a');
+    logo.href = "/";
+    logo.classList.add('logo');
+    
+    sideNav.appendChild(logo);
+
+    const logoText = document.createElement('p');
+    logoText.textContent = 'Ramen House';
+    logoText.classList.add('logo-text');
+
+    logo.appendChild(logoText);
+
+    const siteLocation = document.createElement('div');
+    siteLocation.classList.add('site-location');
+
+    sideNav.appendChild(siteLocation);
+
+    const googleMapLink = document.createElement('a');
+    googleMapLink.href = "https://maps.app.goo.gl/GD9BNZ7fBhJxeE9u6";
+
+    siteLocation.appendChild(googleMapLink);
+
+    const address = ['111 Fake St,', 'San Francisco, CA 12345'];
+
+    for (let i = 0; i < address.length; i++) {
+        const p = document.createElement('p');
+        p.textContent = address[i];
+        googleMapLink.appendChild(p);
+    }
+    
+    const subSideNav = document.createElement('div');
+    subSideNav.classList.add('sub-side-nav');
+    
+    sideNav.appendChild(subSideNav);
+
+    const listItems = ['RESERVE', 'MENU', 'OUR STORY', 'HOURS & LOCATION', 'JOIN OUR TEAM'];
+
+    for (let i = 0; i < listItems.length; i++) {
+        const button = document.createElement('button');
+        button.classList.add(listItems[i].replaceAll(' ', '-').toLowerCase());
+        button.textContent = listItems[i];
+
+        subSideNav.appendChild(button);
+    }
+
+    return navContainer;
+}
+
+export default function preparePage() {
+    document.getElementById('content').appendChild(addNavContainer());
+    document.getElementById('content').classList.add('window');
+}
