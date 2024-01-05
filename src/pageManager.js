b93549f39a5f50b6fe9d56cf4368fdf9ccaf2b25
nzxzn <99358826+nzxzn@users.noreply.github.com>
2024-01-04 16:38:58 -0800

    Add clearContent function so it can refreshes

--- src/pageManager.js
+++ src/pageManager.js
@@ -2,13 +2,32 @@ import prepareNav from './nav.js';
 import prepareMain from './main.js'
 import prepareStory from './our-story.js'
 
+const functionMap = {
+    'main': prepareMain,
+    // 'reserve': prepareReserve,
+    // 'menu': prepareMenu,
+    'our-story': prepareStory,
+    // 'hours-&-location': prepareHours,
+    // 'join-our-team': prepareJoin,
+}
+
+const clearContent = () => {
+    const children = document.getElementsByClassName('window')[0].childNodes;
+    if (children.length > 2) {
+        children[children.length - 1].innerHTML = '';
+    }
+}
+
 export default function loadPage() {
     prepareNav();
+    // prepareMain();
     const buttons = document.querySelector('.sub-side-nav').querySelectorAll('button');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
+            clearContent();
             const className = button.className;
-            console.log(className);
-        })
-    })
+            if (functionMap[className])
+            functionMap[className]();
+        });
+    });
 }
