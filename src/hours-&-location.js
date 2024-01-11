4aee48748cea1e63425b97455efd6940027f7d49
nzxzn <99358826+nzxzn@users.noreply.github.com>
2024-01-10 22:42:58 -0800

    Add hours page

--- src/hours-&-location.js
+++ src/hours-&-location.js
@@ -2,7 +2,18 @@ export default function addHoursPage() {
     const hoursDiv = document.createElement('div');
     hoursDiv.classList.add('hours-location');
 
-    hoursDiv.textContent = 'testing2';
+    const title = document.createElement('p');
+    title.textContent = 'Hours & Location';
+    title.classList.add('title');
+    hoursDiv.appendChild(title);
+
+    const hours = document.createElement('p');
+    hours.textContent = "09:00 am - 09:00 pm Daily";
+    hoursDiv.appendChild(hours);
+
+    const location = document.createElement('p');
+    location.textContent = '111 Fake St, San Francisco, CA 12345';
+    hoursDiv.appendChild(location);
 
     return hoursDiv;
 }
